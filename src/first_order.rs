87183ddf128f7e14da76b4a089662a9ccb550b49
elpinal <elpinal@users.noreply.github.com>
2018-02-10 22:45:15 +0900

    Add Term and Subst

--- src/first_order.rs
+++ src/first_order.rs
@@ -1,4 +1,20 @@
 #[derive(Clone, Debug, PartialEq)]
+pub enum Term {
+    Var(usize),
+    App(Box<Term>, Box<Term>),
+    Abs(Type, Box<Term>),
+    Subst(Box<Term>, Subst),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Subst {
+    Id,
+    Shift,
+    Cons(Box<Term>, Type, Box<Subst>),
+    Compose(Box<Subst>, Box<Subst>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
 pub enum Type {
     K,
     Arr(Box<Type>, Box<Type>),
